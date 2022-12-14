a174d18c5809c05597983fa6cdc1b65a09bf0d96
sreeja <21b01a1220@svecw.edu.in>
2022-12-14 10:42:17 +0530

    first

---
+++ javascript.js
@@ -0,0 +1,30 @@
+function validation(){
+    let name=document.getElementById("NAME") . value;
+    let password=document.getElementById("password") . value;
+    let mailid=document.getElementById("email") . value;
+    let mobilenumber=document.getElementById("mobilenumber") . value;
+    if(name='', password='',mailid='',mobilenumber='')
+    {
+        alert("enter all details")
+        return("true")
+    }
+    if(name="name")
+    {
+        alert("enter valid details")
+    }
+    else{
+        return ("true")
+    }
+    if(name="name")
+    {
+        alert("enter valid mailid")
+    }
+    if(phonenumber=number)
+    {
+        alert("enter valid number")
+    }
+    else{
+        return("true")
+    }
+}
+    
